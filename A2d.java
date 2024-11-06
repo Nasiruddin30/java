33da4790b53951a9fb5d3ef6f7976097ca105a39
Nasiruddin30 <shaiknasiruddin30@gmail.com>
2024-11-06 12:04:09 +0530

    it is a array code.

---
+++ A2d.java
@@ -0,0 +1,16 @@
+public class A2d {
+    public static void main(String[] args) {
+        int [][] number = {
+            {1,2,3,4},
+            {3,4,5,6},
+            {9,7,0,6}
+    };
+    for(int row=0;row<number.length;row++) {
+        for(int columns = 0;columns<number[row].length;columns++) {
+            System.out.print(number[1][1]);
+        }
+        System.out.println();
+    } 
+   
+    }
+}   
